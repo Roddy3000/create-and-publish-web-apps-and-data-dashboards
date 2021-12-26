d41dbeef3c5a752f049c83c92c560f271076912b
Harsh Sharma <92710633+Roddy3000@users.noreply.github.com>
2021-12-26 09:10:01 +0000

    add data

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'Series_1':[1,2,4,5,7]
+  'Series_2':[10,30,40,100,250]
+}
+
+df = pandas.Dataframe(data)
 
 st.title('first streamlit app')
 st.subheader('Introduction')
-st.write('This is our fisrt web app')
+st.write('''This is our fisrt web app
+Enjoy it'''
+)
 
+st.write(df)
